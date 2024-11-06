3b65879431e452ad0052c599ddce4c93fc2b314e
Agatha <amorim.agatha93@outlook.com>
2024-11-05 22:28:14 -0300

    Added constants

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,22 +4,34 @@ describe('Orange HRM Tests', () => {
     usernameField: "[name='username']",
     passwordField: "[name='password']",
     loginButton: '.oxd-button',
-    toppbarField: ".oxd-topbar",
+    dashboardGrid: ".orangehrm-dashboard-grid",
     failtoppbarField:'.oxd-alert'
   }
 
+const userData= {
+  userSucess: {
+    username: 'Admin',
+    password: 'admin123'
+  },
+  userFail:{
+    username: 'test',
+    password: 'test'
+  },
+}
+
+
   it('login - sucess', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorList.usernameField).type ('Admin')
-    cy.get(selectorList.passwordField).type('admin123')
+    cy.get(selectorList.usernameField).type (userData.userSucess.username)
+    cy.get(selectorList.passwordField).type(userData.userSucess.password)
     cy.get(selectorList.loginButton).click ()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorList.toppbarField).contains('Dashboard')
+    cy.get(selectorList.dashboardGrid)
   })
   it('login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorList.usernameField).type ('gath')
-    cy.get(selectorList.passwordField).type('gath123')
+    cy.get(selectorList.usernameField).type (userData.userFail.username)
+    cy.get(selectorList.passwordField).type(userData.userFail.password)
     cy.get(selectorList.loginButton).click ()
     cy.get(selectorList.failtoppbarField)
     
